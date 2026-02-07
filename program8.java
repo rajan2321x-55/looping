0759da790d323ec0ad8b16c54cbcfb9b72bc1117
rajan2321x-55 <rajan2321x@gmail.com>
2026-02-07 10:10:00 +0530

    Remove TODO comments from program8.java
    Removed TODO comments related to prime number check.

---
+++ program8.java
@@ -0,0 +1,32 @@
+package Looping;
+import java.util.Scanner;
+
+public class program8 {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter input:");
+		int n=sc.nextInt();
+		int c=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(i%1==0)
+			{
+				c++;
+			}
+		}
+		if(c==2)
+		{
+			System.out.println("output: prime");
+		}
+		else
+		{
+			System.out.println("output: prime");
+		}
+		
+
+	}
+
+}
